c3d9a313d3b6fa39d36ca2607a9049a5df3a5d70
ChewCEC <camilo.echeverri.100@gmail.com>
2022-11-21 21:43:25 -0500

    Solucion v1

--- pregunta.py
+++ pregunta.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+import datetime
 """
 Limpieza de datos usando Pandas
 -----------------------------------------------------------------------------------------
@@ -6,15 +9,60 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
-import pandas as pd
-
 
 def clean_data():
-
+    
     df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col= 0)
+    df["sexo"] = df["sexo"].str.lower()
+    df["sexo"] = df["sexo"].str.strip()
 
-    #
-    # Inserte su código aquí
-    #
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.lower()
+    df["tipo_de_emprendimiento"] = df["tipo_de_emprendimiento"].str.strip()
+
+    df["idea_negocio"] = df["idea_negocio"].str.lower()
+    df["idea_negocio"] = df["idea_negocio"].str.strip()
+    df["idea_negocio"] = df["idea_negocio"].str.replace("_", " ")
+    df["idea_negocio"] = df["idea_negocio"].str.replace("-", " ")
+    df["idea_negocio"] = df["idea_negocio"].str.replace("__", " ")
+    df["idea_negocio"] = df["idea_negocio"].str.replace("  ", " ")
+
+    df["barrio"] = df["barrio"].str.lower()
+    df["barrio"] = df["barrio"].str.strip()
+    df["barrio"] = df["barrio"].str.replace("_", " ")
+    df["barrio"] = df["barrio"].str.replace("-", " ")
+    df["barrio"] = df["barrio"].str.replace("__", " ")
+    df["barrio"] = df["barrio"].str.replace("  ", " ")
+
+    def clean_currency(x):
+        if isinstance(x, str):
+            return(x.replace('$', '').replace(',', '').replace('.00', ''))
+
+        return(x)
+
+    df['monto_del_credito'] = df['monto_del_credito'].apply(clean_currency).astype('float')
+
+
+    for date in df["fecha_de_beneficio"]:
+        if len(date.split("/")[0]) == 4:
+            date = datetime.datetime.strptime(date, "%Y/%m/%d").strftime("%d/%m/%Y")
+        else:
+            date = datetime.datetime.strptime(date, "%d/%m/%Y")
+
+
+
+    df["línea_credito"] = df["línea_credito"].str.lower()
+    df["línea_credito"] = df["línea_credito"].str.replace("_", " ")
+    df["línea_credito"] = df["línea_credito"].str.replace("-", " ")
+    df["línea_credito"] = df["línea_credito"].str.replace("__", " ")
+
+    df = df.drop_duplicates()
 
     return df
+
+if __name__ == "__main__":
+    print(clean_data().head(0))
+    # print()
+    # print(clean_data()[""].value_counts())
+    # print()
+    #Create a new csv file with the cleaned data
+    clean_data().to_csv("solicitudes_credito_clean.csv", sep= ",")
